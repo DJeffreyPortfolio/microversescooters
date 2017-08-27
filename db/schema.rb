646f9569d5c69a974489f24011f89b2ba67105f0
djeffreyportfolio <d_jeffrey87@yahoo.com>
2017-08-27 15:24:46 -0400

    4.2 added changes of color and price

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170827130410) do
+ActiveRecord::Schema.define(version: 20170827141836) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170827130410) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "color"
+    t.float "price"
   end
 
 end
