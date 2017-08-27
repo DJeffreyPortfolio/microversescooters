6eb301e13951d9efe17aa1d1d75a5b4b765577e6
djeffreyportfolio <d_jeffrey87@yahoo.com>
2017-08-27 16:07:12 -0400

    4.1 added seed changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.create(name: "Pink Vesper", description: "Beautiful pink Vesper to drive around on!", image_url: "https://s-media-cache-ak0.pinimg.com/originals/04/fc/f4/04fcf4ca81f9a4c6e2b44b9ad2945cde.jpg",
+	color: "Pink", price: "289")
