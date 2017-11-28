a8b573b1c525d135c7fefc107b0d42f3e626676e
DJeffreyPortfolio <djeffrey87@yahoo.com>
2017-11-28 09:41:26 -0500

    5.9 changes made to product_controller and show.html.erb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,7 +14,8 @@ class ProductsController < ApplicationController
 
   # GET /products/1
   # GET /products/1.json
-  def show
+  def show    
+    @comments = @product.comments.paginate(page: params[:page], per_page: 5)
     @comments = @product.comments.order("created_at DESC")
   end
 
