03481a597b434bfb491b281514f9a9fdbf7e1232
DJeffreyPortfolio <djeffrey87@yahoo.com>
2018-01-02 15:03:52 -0500

    Added function testing spec files

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -2,19 +2,29 @@ require 'rails_helper'
 
 describe UsersController, type: :controller do
 	
-	let(:user) {User.create!(email: 'test@testing.com', password: 'testingtesting')}
+	let(:user1) {User.create!(email: 'test@testing.com', password: 'testingtesting')}
+	let(:user2) {User.create!(email: 'test2@testing.com', password: 'testingtesting')}
+
 
 	describe 'GET #show' do
-		contest 'when a user is logged in' do
+		context 'when a user is logged in' do
 			before do
-				sign_in user
+				sign_in user1
 			end
 		end
 
 		context 'when a user is not logged in' do
 			it 'redirects to login' do
-				get :show, params: {id: user.id}
+				get :show, params: {id: user1.id}
 				expect(response).to redirect_to(new_user_session_path)
+			end
+		end
+
+		context 'user trying to view another profile' do
+			get :show, params: {id: user2.id}
+			it 'redirects to root url and throws execption' do
+				expect(response).to redirect_to (root_path)
+				expect(response).to have_http_status(302)
 			end
 		end
 	end
