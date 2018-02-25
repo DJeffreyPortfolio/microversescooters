3c80a91fcc742a55eff9cbd2500c6ee94ac5b668
DJeffreyPortfolio <djeffrey87@yahoo.com>
2018-02-25 11:33:39 -0500

    6.14 changes made to comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,8 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :product
 
-  scope :rating_ascending, ->{reorder(rating: :ascending)}
+  scope :rating_desc, ->{order(rating: :desc)}
+  scope :rating_asc, ->{order(rating: :asc)}
 
   validates :body, presence: true
   validates :user, presence: true
